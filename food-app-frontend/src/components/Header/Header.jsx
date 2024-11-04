768c2fd65d5afb9e1bc540ca5c2d3a9fe428d13e
codev-001 <codev077@gmail.com>
2024-11-04 23:09:17 +0530

    updated UI

--- food-app-frontend/src/components/Header/Header.jsx
+++ food-app-frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favorite food here</h2>
             <p>choose from the diverse menu which offers every cuisine in the world whether it is indian , chinese or italian.</p>
-            <button>view menu</button>
+            <button><a href="#explore-menu">view menu</a></button>
         </div>
     </div>
   )
